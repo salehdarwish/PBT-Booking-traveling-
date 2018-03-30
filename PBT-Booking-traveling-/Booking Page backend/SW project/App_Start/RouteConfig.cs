a7e3319a0a4dbfc3189999827ed8509e4f05475e
saleh <salehdarwish.97@gmail.com>
2018-03-30 02:51:14 +0200

    Booking page backend

---
+++ PBT-Booking-traveling-/Booking Page backend/SW project/App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SW_project
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+
+               routes.MapRoute(
+                name: null,
+                url: "",
+                defaults: new { controller = "Booking", action = "Index"}
+            );
+
+               routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Booking", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
